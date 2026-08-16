21ec935a27f72a8f6da922da6278814faa9e7594
Dawid Stasinski <davis@interia.pl>
2026-08-16 19:47:57 +0200

    Dodanie automatycznego HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: ['172.18.0.0/16']);
         $middleware->web(append: [EnsurePasswordIsCurrent::class]);
         $middleware->alias(['role' => EnsureUserHasRole::class]);
     })
